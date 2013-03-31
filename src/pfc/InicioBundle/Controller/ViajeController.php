3dc3ad47762ddfdc54b794dd460f2c37c2eb0e1d
kraumor <kraumor@gmail.com>
2013-03-31 21:56:01 +0200

    ACL viajes+fixtures

--- src/pfc/InicioBundle/Controller/ViajeController.php
+++ src/pfc/InicioBundle/Controller/ViajeController.php
@@ -4,30 +4,33 @@ namespace pfc\InicioBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use pfc\InicioBundle\Entity\Viaje;
 use pfc\InicioBundle\Form\ViajeType;
+use Symfony\Component\Security\Acl\Domain\ObjectIdentity,
+    Symfony\Component\Security\Acl\Domain\UserSecurityIdentity,
+    Symfony\Component\Security\Acl\Permission\MaskBuilder;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
  * Viaje controller.
  *
  */
-class ViajeController extends Controller
-{
+class ViajeController extends Controller {
+
     /**
      * Lists all Viaje entities.
      *
      */
-    public function listarAction()
-    {
-        $em = $this->getDoctrine()->getManager();
+    public function listarAction() {
+        $em=$this->getDoctrine()->getManager();
         $usuario=$this->get('security.context')->getToken()->getUser();
-        
+
 //        $entities = $em->getRepository('InicioBundle:Viaje')->findAll();
         $entities=$em->getRepository('InicioBundle:Usuario')->findViajesRecientes($usuario->getId());
 
-        return $this->render('InicioBundle:Viaje:listar.html.twig', array(
-            'entities' => $entities,
+        return $this->render('InicioBundle:Viaje:listar.html.twig',array(
+                    'entities' => $entities,
         ));
     }
 
@@ -35,28 +38,35 @@ class ViajeController extends Controller
      * Creates a new Viaje entity.
      *
      */
-    public function crearAction(Request $request)
-    {
-        $entity  = new Viaje();
-        $form = $this->createForm(new ViajeType(), $entity);
-        $usuario=$this->get('security.context')->getToken()->getUser();
+    public function crearAction(Request $request) {
         
+        $entity=new Viaje();
+        $form=$this->createForm(new ViajeType(),$entity);
+        $usuario=$this->get('security.context')->getToken()->getUser();
+
         $form->bind($request);
 
-        if ($form->isValid()) {
-            
+        if($form->isValid()){
+
             $entity->setUsuario($usuario)
-                   ->setFecha(new \DateTime());
-            $em = $this->getDoctrine()->getManager();
+                    ->setFecha(new \DateTime());
+            $em=$this->getDoctrine()->getManager();
             $em->persist($entity);
             $em->flush();
 
-            return $this->redirect($this->generateUrl('viaje_mostrar', array('id' => $entity->getId())));
+            //ACL
+            $idObjeto=ObjectIdentity::fromDomainObject($entity);
+            $idUsuario=UserSecurityIdentity::fromAccount($usuario);
+            $acl=$this->get('security.acl.provider')->createAcl($idObjeto);
+            $acl->insertObjectAce($idUsuario,MaskBuilder::MASK_OPERATOR);
+            $this->get('security.acl.provider')->updateAcl($acl);
+
+            return $this->redirect($this->generateUrl('viaje_mostrar',array('id' => $entity->getId())));
         }
 
-        return $this->render('InicioBundle:Viaje:crear.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
+        return $this->render('InicioBundle:Viaje:crear.html.twig',array(
+                    'entity' => $entity,
+                    'form' => $form->createView(),
         ));
     }
 
@@ -64,14 +74,14 @@ class ViajeController extends Controller
      * Displays a form to create a new Viaje entity.
      *
      */
-    public function nuevoAction()
-    {
-        $entity = new Viaje();
-        $form   = $this->createForm(new ViajeType(), $entity);
+    public function nuevoAction() {
+        
+        $entity=new Viaje();
+        $form=$this->createForm(new ViajeType(),$entity);
 
-        return $this->render('InicioBundle:Viaje:crear.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
+        return $this->render('InicioBundle:Viaje:crear.html.twig',array(
+                    'entity' => $entity,
+                    'form' => $form->createView(),
         ));
     }
 
@@ -79,44 +89,53 @@ class ViajeController extends Controller
      * Finds and displays a Viaje entity.
      *
      */
-    public function mostrarAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
+    public function mostrarAction($id) {
+        
+        $em=$this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('InicioBundle:Viaje')->find($id);
+        $entity=$em->getRepository('InicioBundle:Viaje')->find($id);
 
-        if (!$entity) {
+        //comprueba permiso ACL
+        if(false===$this->get('security.context')->isGranted('VIEW',$entity)){
+            throw new AccessDeniedException();
+        }
+
+        if(!$entity){
             throw $this->createNotFoundException('Viaje no encontrado.');
         }
 
-        $deleteForm = $this->createDeleteForm($id);
+        $deleteForm=$this->createDeleteForm($id);
 
-        return $this->render('InicioBundle:Viaje:mostrar.html.twig', array(
-            'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),        ));
+        return $this->render('InicioBundle:Viaje:mostrar.html.twig',array(
+                    'entity' => $entity,
+                    'delete_form' => $deleteForm->createView(),));
     }
 
     /**
      * Displays a form to edit an existing Viaje entity.
      *
      */
-    public function editarAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
+    public function editarAction($id) {
+        
+        $em=$this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('InicioBundle:Viaje')->find($id);
+        $entity=$em->getRepository('InicioBundle:Viaje')->find($id);
 
-        if (!$entity) {
+        if(false===$this->get('security.context')->isGranted('EDIT',$entity)){
+            throw new AccessDeniedException();
+        }
+        
+        if(!$entity){
             throw $this->createNotFoundException('Viaje no encontrado.');
         }
 
-        $editForm = $this->createForm(new ViajeType(), $entity);
-        $deleteForm = $this->createDeleteForm($id);
+        $editForm=$this->createForm(new ViajeType(),$entity);
+        $deleteForm=$this->createDeleteForm($id);
 
-        return $this->render('InicioBundle:Viaje:editar.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+        return $this->render('InicioBundle:Viaje:editar.html.twig',array(
+                    'entity' => $entity,
+                    'edit_form' => $editForm->createView(),
+                    'delete_form' => $deleteForm->createView(),
         ));
     }
 
@@ -124,31 +143,35 @@ class ViajeController extends Controller
      * Edits an existing Viaje entity.
      *
      */
-    public function actualizarAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
+    public function actualizarAction(Request $request,$id) {
+        
+        $em=$this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('InicioBundle:Viaje')->find($id);
+        $entity=$em->getRepository('InicioBundle:Viaje')->find($id);
 
-        if (!$entity) {
+        if(false===$this->get('security.context')->isGranted('EDIT',$entity)){
+            throw new AccessDeniedException();
+        }
+        
+        if(!$entity){
             throw $this->createNotFoundException('Viaje no encontrado.');
         }
 
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createForm(new ViajeType(), $entity);
+        $deleteForm=$this->createDeleteForm($id);
+        $editForm=$this->createForm(new ViajeType(),$entity);
         $editForm->bind($request);
 
-        if ($editForm->isValid()) {
+        if($editForm->isValid()){
             $em->persist($entity);
             $em->flush();
 
-            return $this->redirect($this->generateUrl('viaje_editar', array('id' => $id)));
+            return $this->redirect($this->generateUrl('viaje_editar',array('id' => $id)));
         }
 
-        return $this->render('InicioBundle:Viaje:editar.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+        return $this->render('InicioBundle:Viaje:editar.html.twig',array(
+                    'entity' => $entity,
+                    'edit_form' => $editForm->createView(),
+                    'delete_form' => $deleteForm->createView(),
         ));
     }
 
@@ -156,16 +179,20 @@ class ViajeController extends Controller
      * Deletes a Viaje entity.
      *
      */
-    public function borrarAction(Request $request, $id)
-    {
-        $form = $this->createDeleteForm($id);
+    public function borrarAction(Request $request,$id) {
+        
+        $form=$this->createDeleteForm($id);
         $form->bind($request);
+        
+        if($form->isValid()){
+            $em=$this->getDoctrine()->getManager();
+            $entity=$em->getRepository('InicioBundle:Viaje')->find($id);
 
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('InicioBundle:Viaje')->find($id);
-
-            if (!$entity) {
+            if(false===$this->get('security.context')->isGranted('DELETE',$entity)){
+                throw new AccessDeniedException();
+            }      
+            
+            if(!$entity){
                 throw $this->createNotFoundException('Viaje no encontrado.');
             }
 
@@ -183,11 +210,11 @@ class ViajeController extends Controller
      *
      * @return Symfony\Component\Form\Form The form
      */
-    private function createDeleteForm($id)
-    {
+    private function createDeleteForm($id) {
         return $this->createFormBuilder(array('id' => $id))
-            ->add('id', 'hidden')
-            ->getForm()
+                        ->add('id','hidden')
+                        ->getForm()
         ;
     }
+
 }
--- src/pfc/InicioBundle/DataFixtures/ORM/ViajeFixtures.php
+++ src/pfc/InicioBundle/DataFixtures/ORM/ViajeFixtures.php
@@ -6,53 +6,72 @@ use pfc\InicioBundle\Entity\Viaje;
 use \Doctrine\Common\DataFixtures\AbstractFixture;
 use \Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-
-//****
-use Doctrine\Common\DataFixtures\FixtureInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-//***
-class ViajeFixtures extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
-{
+use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
+use Symfony\Component\Security\Acl\Domain\UserSecurityIdentity;
+use Symfony\Component\Security\Acl\Permission\MaskBuilder;
+
+class ViajeFixtures extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface {
+
     private $container;
-    
-    
-    public function getOrder()
-    {
+
+    public function setContainer(ContainerInterface $container=null) {
+
+        $this->container=$container;
+    }
+
+    public function getOrder() {
         return 2;
     }
-    
-    public function load(ObjectManager $manager)
-    {
-        $viajes = array(
-            array('nombre' => 'viatge11', 'fecha' => '2002-05-11 11:11:11', 'usuario_id' => '1' )
-           ,array('nombre' => 'viatge21', 'fecha' => '2002-03-11 11:11:11', 'usuario_id' => '2' )
-           ,array('nombre' => 'viatge22', 'fecha' => '2002-02-11 11:11:11', 'usuario_id' => '2' )
-           ,array('nombre' => 'viatge31', 'fecha' => '2002-04-11 11:11:11', 'usuario_id' => '3' )
-           ,array('nombre' => 'viatge32', 'fecha' => '2002-05-11 11:11:11', 'usuario_id' => '3' )
-           ,array('nombre' => 'viatge33', 'fecha' => '2002-01-11 11:11:11', 'usuario_id' => '3' )
-           ,array('nombre' => 'viatge41', 'fecha' => '2002-06-11 11:11:11', 'usuario_id' => '4' )
+
+    public function load(ObjectManager $manager) {
+
+        $viajes=array(
+            array('nombre' => 'viatge11','fecha' => '2002-05-11 11:11:11','usuario_id' => '1')
+            ,array('nombre' => 'viatge21','fecha' => '2002-03-11 11:11:11','usuario_id' => '2')
+            ,array('nombre' => 'viatge22','fecha' => '2002-02-11 11:11:11','usuario_id' => '2')
+            ,array('nombre' => 'viatge31','fecha' => '2002-04-11 11:11:11','usuario_id' => '3')
+            ,array('nombre' => 'viatge32','fecha' => '2002-05-11 11:11:11','usuario_id' => '3')
+            ,array('nombre' => 'viatge33','fecha' => '2002-01-11 11:11:11','usuario_id' => '3')
+            ,array('nombre' => 'viatge41','fecha' => '2002-06-11 11:11:11','usuario_id' => '4')
         );
- 
-        foreach ($viajes as $v) {
-            
+
+        foreach($viajes as $v){
+
             $usuario=$manager->getRepository('InicioBundle:Usuario')->find($v['usuario_id']);
-            
-            $viaje = new Viaje();
+
+            $viaje=new Viaje();
 
             $viaje->setNombre($v['nombre'])
-                  ->setFecha(new \DateTime($v['fecha']))
-                  ->setUsuario($usuario)
-                ;
- 
+                    ->setFecha(new \DateTime($v['fecha']))
+                    ->setUsuario($usuario)
+            ;
+
             $manager->persist($viaje);
+            //flush ahora, lo necesita el acl
+            $manager->flush();
+
+            //asignar ACL
+            $proveedor=$this->container->get('security.acl.provider');
+
+            $idObjeto=ObjectIdentity::fromDomainObject($viaje);
+            $idUsuario=UserSecurityIdentity::fromAccount($usuario);
+
+            //busca ACL del par (viaje usuario) y si no tiene, lo crea
+            try{
+                $acl=$proveedor->findAcl($idObjeto,array($idUsuario));
+            } catch (\Symfony\Component\Security\Acl\Exception\AclNotFoundException $e){
+                $acl=$proveedor->createAcl($idObjeto);
+            }
+            //borrar ACES previos
+            $aces=$acl->getObjectAces();
+            foreach($aces as $index => $ace){
+                $acl->deleteObjectAce($index);
+            }
+            //insertar ACE
+            $acl->insertObjectAce($idUsuario,MaskBuilder::MASK_OPERATOR);
+            $proveedor->updateAcl($acl);
         }
- 
-        $manager->flush();
-    }
-
-    public function setContainer(ContainerInterface $container = null) {
-
-        $this->container = $container;
     }
 }
